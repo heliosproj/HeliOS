562b9f147a282d14000de316c4d3db7c86163930
Manny Peterson <me@mannypeterson.com>
2020-08-18 13:21:25 -0500

    adding new example Arduino sketches

---
+++ examples/Coop/Coop.ino
@@ -0,0 +1,109 @@
+/*
+ * HeliOS Embedded Operating System
+ * Copyright (C) 2020 Manny Peterson <me@mannypeterson.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Additional documentation on HeliOS and its Application
+ * Programming Interface (API) is available in the
+ * HeliOS Programmer's Guide which can be found here:
+ *
+ * https://github.com/MannyPeterson/HeliOS/blob/master/extras/HeliOS_Programmers_Guide.md
+ */
+
+/*
+ * Include the standard HeliOS header for Arduino sketches. This header
+ * includes the required HeliOS header files automatically.
+ */
+#include <HeliOS_Arduino.h>
+
+/*
+ * The task definition for taskShort() which
+ * will perform 100,000 arbitrary floating point
+ * operations.
+ */
+void taskShort(int id_) {
+  float a = 0.0f;
+
+  for(int i = 0; i < 100 000; i++) {
+    a *= 3.14f;
+  }
+}
+
+/*
+ * The task definition for taskLong() which
+ * will perform 1,000,000 arbitrary floating point
+ * operations.
+ */
+void taskLong(int id_) {
+  float a = 0.0f;
+
+  for(int i = 0; i < 1 000 000; i++) {
+    a *= 3.14f;
+  }
+}
+
+void setup() {
+
+  /*
+   * Declare and initialize an int to hold the
+   * task id.
+   */
+  int id = 0;
+
+  /*
+   * Call xHeliOSSetup() to initialize HeliOS and
+   * its data structures. xHeliOSSetup() must be
+   * called before any other HeliOS function call.
+   */
+  xHeliOSSetup();
+
+  /*
+   * Add the task taskShort() to HeliOS by passing
+   * xTaskAdd() the friendly name of the task as well
+   * as a callback pointer to the task function.
+   */
+  id = xTaskAdd("TASKSHORT", &taskShort);
+
+  /*
+   * Call xTaskStart() to start taskShort() by passing
+   * xTaskStart() the id of the task to start.
+   */
+  xTaskStart(id);
+
+  /*
+   * Add the task taskShort() to HeliOS by passing
+   * xTaskAdd() the friendly name of the task as well
+   * as a callback pointer to the task function.
+   */
+  id = xTaskAdd("TASKLONG", &taskLong);
+
+  /*
+   * Call xTaskStart() to start taskLong() by passing
+   * xTaskStart() the id of the task to start.
+   */
+  xTaskStart(id);
+}
+
+void loop() {
+
+  /*
+   * Momentarily pass control to HeliOS by calling the
+   * xHeliOSLoop() function call. xHeliOSLoop() should be
+   * the only code inside of the sketch's loop() function.
+   */
+  xHeliOSLoop();
+}
---
+++ examples/ListTasks/ListTasks.ino
@@ -0,0 +1,165 @@
+/*
+ * HeliOS Embedded Operating System
+ * Copyright (C) 2020 Manny Peterson <me@mannypeterson.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Additional documentation on HeliOS and its Application
+ * Programming Interface (API) is available in the
+ * HeliOS Programmer's Guide which can be found here:
+ *
+ * https://github.com/MannyPeterson/HeliOS/blob/master/extras/HeliOS_Programmers_Guide.md
+ */
+
+/*
+ * Include the standard HeliOS header for Arduino sketches. This header
+ * includes the required HeliOS header files automatically.
+ */
+#include <HeliOS_Arduino.h>
+
+/*
+ * The task definition for taskSerial() which will
+ * be executed by HeliOS every 1,000,000 microseconds
+ * (1 second).
+ */
+void taskSerial(int id_) {
+
+  /*
+   * Declare and initialize a string object to
+   * hold the text which will be written to the
+   * serial bus every 1,000,000 microseconds
+   * (1 second).
+   */
+  String str = "";
+
+  /*
+   * Declare and initialize and int to store the number
+   * of tasks in the task list. This will get populated
+   * by xTaskGetList().
+   */
+  int tasks = 0;
+
+  /*
+   * Call xTaskGetInfo() to obtain the task information
+   * by passing xTaskGetInfo() the task id of the active
+   * task.
+   */
+  xTaskGetListResult* tres = xTaskGetList(&tasks);
+
+  /*
+   * Check the pointer to the xTaskGetListResult
+   * structure before accessing any of its members
+   * since xTaskGetList() can return null if the
+   * task id does not exist or HeliOS is unable
+   * to reserve the required managed memory.
+   */
+  if(tres) {
+
+    /*
+     * Loop through the xTaskGetListResult array
+     * for the number of times specified by
+     * the int task.
+     */
+    for(int i = 0; i < tasks; i++) {
+      /*
+       * Clear the string.
+       */
+      str = "";
+
+      /*
+       * Append all of the members of the xTaskGetListResult
+       * structure to the string.
+       */
+      str += "taskSerial(): id = ";
+      str += tres->id;
+      str += ", name = ";
+      str += tres->name;
+      str += ", state = ";
+      str += tres->state;
+      str += ", ltime = ";
+      str += tres->lastRuntime;
+      str += ", ttime = ";
+      str += tres->totalRuntime;
+
+      /*
+       * Print the string to the serial bus.
+       */
+      Serial.println(str);
+    }
+
+  }
+
+  /*
+   * Free the managed memory allocated by the xTaskGetInfo()
+   * function call. If xMemFree() is not called, HeliOS
+   * may exhaust its available managed memory through
+   * subsequent calls to xTaskGetInfo().
+   */
+  xMemFree(tres);
+}
+
+void setup() {
+
+  /*
+   * Declare and initialize an int to hold the
+   * task id.
+   */
+  int id = 0;
+
+  /*
+   * Call xHeliOSSetup() to initialize HeliOS and
+   * its data structures. xHeliOSSetup() must be
+   * called before any other HeliOS function call.
+   */
+  xHeliOSSetup();
+
+  /*
+   * Set the serial data rate and begin serial
+   * communication.
+   */
+  Serial.begin(9600);
+
+  /*
+   * Add the task taskSerial() to HeliOS by passing
+   * xTaskAdd() the friendly name of the task as well
+   * as a callback pointer to the task function.
+   */
+  id = xTaskAdd("TASKSERIAL", &taskSerial);
+
+  /*
+   * Call xTaskWait() to place taskSerial() into a wait
+   * state by passing xTaskWait() the task id. A task
+   * must be in a wait state to respond to timer events.
+   */
+  xTaskWait(id);
+
+  /*
+   * Set the timer interval for taskSerial() to 1,000,000 microseconds
+   * (1 second). HeliOS automatically begins incrementing
+   * the timer for the task once the timer interval is set.
+   */
+  xTaskSetTimer(id, 1000000);
+}
+
+void loop() {
+
+  /*
+   * Momentarily pass control to HeliOS by calling the
+   * xHeliOSLoop() function call. xHeliOSLoop() should be
+   * the only code inside of the sketch's loop() function.
+   */
+  xHeliOSLoop();
+}
