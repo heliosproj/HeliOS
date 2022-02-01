ad5ed69e44c477ae2764cfa05f2db96aefbbe479
Manny Peterson <12462046+MannyPeterson@users.noreply.github.com>
2022-01-31 19:47:58 -0600

    started implementing doxygen

--- src/task.h
+++ src/task.h
@@ -1,19 +1,26 @@
-/*
+/**
+ * @file task.h
+ * @author Manny Peterson (mannymsp@gmail.com)
+ * @brief Source code for tasks and managing tasks in HeliOS
+ * @version 0.3.0
+ * @date 2022-01-31
+ * 
  * HeliOS Embedded Operating System
  * Copyright (C) 2020-2022 Manny Peterson <mannymsp@gmail.com>
- *
+ *  
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ *  
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ *  
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
  */
 #ifndef TASK_H_
 #define TASK_H_
--- src/timer.h
+++ src/timer.h
@@ -1,19 +1,26 @@
-/*
+/**
+ * @file timer.h
+ * @author Manny Peterson (mannymsp@gmail.com)
+ * @brief Source code for timers and managing timers in HeliOS
+ * @version 0.3.0
+ * @date 2022-01-31
+ * 
  * HeliOS Embedded Operating System
  * Copyright (C) 2020-2022 Manny Peterson <mannymsp@gmail.com>
- *
+ *  
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ *  
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ *  
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
  */
 #ifndef TIMER_H_
 #define TIMER_H_
