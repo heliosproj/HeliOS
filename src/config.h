f3a32f1f8a98831bc747d39ef42f0f12778919b6
Manny Peterson <12462046+MannyPeterson@users.noreply.github.com>
2022-02-06 16:51:17 -0600

    update

--- src/config.h
+++ src/config.h
@@ -29,20 +29,73 @@
 /* The following configurable settings may be changed by the end-user
 to customize HeliOS for their specific use. */
 
-/* Define the size in bytes of the queue message value. */
+/**
+ * @brief Define the size in bytes of the message queue message value.
+ * 
+ * Setting the CONFIG_MESSAGE_VALUE_BYTES allows the end-user to define
+ * the size of the message queue message value. The larger the size of the
+ * message value, the greater impact there will be on system performance.
+ * The default size is 16.
+ * 
+ * @sa xQueueMessage
+ * 
+ */
 #define CONFIG_MESSAGE_VALUE_BYTES 16
 
-/* Define the size in bytes of the task notification value. */
+/**
+ * @brief Define the size in bytes of the direct to task notification value.
+ * 
+ * Setting the CONFIG_NOTIFICATION_VALUE_BYTES allows the end-user to define
+ * the size of the direct to task notification value. The larger the size of the
+ * notification value, the greater impact there will be on system performance.
+ * The default size is 16.
+ * 
+ * @sa xTaskNotification
+ */
 #define CONFIG_NOTIFICATION_VALUE_BYTES 16
 
-/* Define the size in bytes of the task name. */
+/**
+ * @brief Define the size in bytes of the ASCII task name.
+ * 
+ * Setting the CONFIG_TASK_NAME_BYTES allows the end-user to define
+ * the size of the ASCII task name. The larger the size of the task
+ * name, the greater impact there will be on system performance.
+ * The default size is 16.
+ * 
+ * @sa xTaskInfo
+ * 
+ */
 #define CONFIG_TASK_NAME_BYTES 16
 
-/* Define the number of entries in the dynamic memory allocation table. */
+/**
+ * @brief Define the number of entries in the dynamic memory allocation table.
+ * 
+ * Setting CONFIG_DYNAMIC_MEMORY_ALLOC_TABLE_ENTRIES allows the end-user to define
+ * the size of the table that is used to track dynamic memory allocated and freed
+ * by xMemAlloc() and xMemFree(). The larger the table, the greater the impact
+ * there will be on system performance. However, if the table is too small,
+ * xMemAlloc() may exhaust the available table entries and fail to allocate
+ * the requested memory.
+ * 
+ * @sa xMemAlloc()
+ * @sa xMemFree()
+ * 
+ */
 #define CONFIG_DYNAMIC_MEMORY_ALLOC_TABLE_ENTRIES 100
 
-/* When the messages in a message queue reach the limit set when the queue is
-created, the queue is considered full. This defines the minimum value for
-"limit" when a queue is created. */
+/**
+ * @brief Define the minimum value for a message queue limit.
+ * 
+ * Setting the CONFIG_QUEUE_MINIMUM_LIMIT allows the end-user to define
+ * the MINIMUM value a message queue can be created with xQueueCreate().
+ * When a message queue length equals its limit, the message queue will
+ * be considered full and return true when xQueueIsQueueFull() is called.
+ * A full queue will also not accept messages from xQueueSend().
+ * 
+ * @sa xQueueIsQueueFull()
+ * @sa xQueueSend()
+ * @sa xQueueCreate()
+ * 
+ */
 #define CONFIG_QUEUE_MINIMUM_LIMIT 5
 #endif
