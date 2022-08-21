abb9f2bd07d95d9bad9439bdbf06256ac54a4c8a
Manny Peterson <12462046+MannyPeterson@users.noreply.github.com>
2022-08-21 13:52:29 -0500

    unit test updates

--- extras/test/src/test.c
+++ extras/test/src/test.c
@@ -24,14 +24,20 @@
  *
  */
 
+#include <posix.h>
+
 #include <HeliOS.h>
 
+#include <stdio.h>
+
+void task_main(xTask task_, xTaskParm parm_) {
+
+  return;
+}
 
 int main(int argc, char **argv) {
 
-
   xBase ret = zero;
-
 
   /* BEGIN: MEMORY UNIT TESTS */
 
@@ -39,17 +45,94 @@ int main(int argc, char **argv) {
 
   mem01 = (xBase *)xMemAlloc(0x32000u);
   if (ISNULLPTR(mem01)) {
-    /* ret++; */
-  }
-
-  if (0x32000u != xMemGetUsed()) {
     ret++;
   }
 
-
-  if (0x32000u != xMemGetSize(mem01)) {
-    /*ret++; */
+  if (0x32020u != xMemGetUsed()) {
+    ret++;
   }
+
+  if (0x32020u != xMemGetSize(mem01)) {
+    ret++;
+  }
+
+  xMemoryRegionStats mem02 = NULL;
+  xMemoryRegionStats mem03 = NULL;
+
+  mem02 = xMemGetHeapStats();
+  if (ISNULLPTR(mem02)) {
+    ret++;
+  }
+
+  if (0x63A0u != mem02->availableSpaceInBytes) {
+    ret++;
+  }
+
+  if (0x63A0u != mem02->largestFreeEntryInBytes) {
+    ret++;
+  }
+
+  if (0x63A0u != mem02->minimumEverFreeBytesRemaining) {
+    ret++;
+  }
+
+  if (0x31Du != mem02->numberOfFreeBlocks) {
+    ret++;
+  }
+
+  if (0x63A0u != mem02->smallestFreeEntryInBytes) {
+    ret++;
+  }
+
+  if (0x2u != mem02->successfulAllocations) {
+    ret++;
+  }
+
+  if (0x0u != mem02->successfulFrees) {
+    ret++;
+  }
+
+  xTask mem04 = NULL;
+
+  mem04 = xTaskCreate("NONE", task_main, NULL);
+  xTaskDelete(mem04);
+
+  mem03 = xMemGetKernelStats();
+  if (ISNULLPTR(mem03)) {
+    ret++;
+  }
+
+  if (0x383C0u != mem03->availableSpaceInBytes) {
+    ret++;
+  }
+
+  if (0x383C0u != mem03->largestFreeEntryInBytes) {
+    ret++;
+  }
+
+  if (0x38340u != mem03->minimumEverFreeBytesRemaining) {
+    ret++;
+  }
+
+  if (0x1C1Eu != mem03->numberOfFreeBlocks) {
+    ret++;
+  }
+
+  if (0x383C0u != mem03->smallestFreeEntryInBytes) {
+    ret++;
+  }
+
+  if (0x2u != mem03->successfulAllocations) {
+    ret++;
+  }
+
+  if (0x1u != mem03->successfulFrees) {
+    ret++;
+  }
+
+  xMemFree(mem01);
+  xMemFree(mem02);
+  xMemFree(mem03);
 
   /* END: MEMORY UNIT TESTS */
 
