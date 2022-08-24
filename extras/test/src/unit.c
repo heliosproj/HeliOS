3c9f98b74a5ce50fee69cf040844eb152d516400
Manny Peterson <12462046+MannyPeterson@users.noreply.github.com>
2022-08-23 21:50:55 -0500

    unit test updates

--- extras/test/src/unit.c
+++ extras/test/src/unit.c
@@ -2,26 +2,26 @@
  * @file unit.c
  * @author Manny Peterson (mannymsp@gmail.com)
  * @brief A simple unit testing framework for HeliOS
- * @version 1.0.0
+ * @version 0.3.5
  * @date 2022-08-23
- *
+ * 
  * @copyright
  * HeliOS Embedded Operating System
  * Copyright (C) 2020-2022 Manny Peterson <mannymsp@gmail.com>
- *
+ *  
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ *  
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ *  
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
+ * 
  */
 #include "unit.h"
 
