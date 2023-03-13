ceadd1215c4b2576cd16d7f47a0d27d529400638
Manny Peterson <12462046+MannyPeterson@users.noreply.github.com>
2023-03-13 12:39:06 -0500

    optimizations to consistency checker

--- src/mem.c
+++ src/mem.c
@@ -172,17 +172,16 @@ static Return_t __calloc__(volatile MemoryRegion_t *region_, volatile Addr_t **a
 static Return_t __free__(volatile MemoryRegion_t *region_, const volatile Addr_t *addr_);
 static Return_t __MemGetRegionStats__(const volatile MemoryRegion_t *region_, MemoryRegionStats_t **stats_);
 static Return_t __DefragMemoryRegion__(const volatile MemoryRegion_t *region_);
+static Return_t __MemoryRegionInit__(volatile MemoryRegion_t *region_);
 
 
 Return_t __MemoryInit__(void) {
   RET_DEFINE;
 
-  /* Call __memset__() on the heap and kernel memory regions to clear (i.e.,
-   * zero out) them then set the starting memory statistics for both regions.*/
-  if(OK(__memset__(&heap, 0x0, sizeof(MemoryRegion_t)))) {
-    if(OK(__memset__(&kernel, 0x0, sizeof(MemoryRegion_t)))) {
-      heap.minAvailableEver = CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS * CONFIG_MEMORY_REGION_BLOCK_SIZE;
-      kernel.minAvailableEver = CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS * CONFIG_MEMORY_REGION_BLOCK_SIZE;
+
+  /* Initialize the heap and kernel memory regions. */
+  if(OK(__MemoryRegionInit__(&heap))) {
+    if(OK(__MemoryRegionInit__(&kernel))) {
       RET_OK;
     } else {
       ASSERT;
@@ -247,6 +246,7 @@ Return_t xMemGetUsed(Size_t *size_) {
     if(OK(__MemoryRegionCheck__(&heap, null, MEMORY_REGION_CHECK_OPTION_WO_ADDR))) {
       cursor = heap.start;
 
+
       /* Traverse, while the cursor is not null, the memory entries in the heap
        * memory region and add up the in-use blocks as we go. */
       while(NOTNULLPTR(cursor)) {
@@ -258,6 +258,7 @@ Return_t xMemGetUsed(Size_t *size_) {
 
         cursor = cursor->next;
       }
+
 
       /* We need to give the user back bytes, not blocks, so multiply the in-use
        * blocks by the block size in bytes. */
@@ -289,6 +290,7 @@ Return_t xMemGetSize(const volatile Addr_t *addr_, Size_t *size_) {
        * allocated memory pointed to by the address pointer. */
       tosize = ADDR2ENTRY(addr_, &heap);
 
+
       /* If the memory entry pointed to by tosize is *NOT* free, then give the
        * user back the number of bytes in-use by multiply the blocks contained
        * in the entry by the block size in bytes. */
@@ -313,125 +315,110 @@ static Return_t __MemoryRegionCheck__(const volatile MemoryRegion_t *region_, co
   RET_DEFINE;
 
 
-  MemoryEntry_t *cursor = null;
-  MemoryEntry_t *find = null;
   Base_t found = false;
   HalfWord_t blocks = zero;
+  MemoryEntry_t *find = null;
+  MemoryEntry_t *cursor = region_->start;
 
 
-  if((NOTNULLPTR(region_) && NULLPTR(addr_) && (MEMORY_REGION_CHECK_OPTION_WO_ADDR == option_)) || (NOTNULLPTR(region_) && NOTNULLPTR(addr_) &&
-    (MEMORY_REGION_CHECK_OPTION_W_ADDR == option_))) {
-    /* Check to make sure a previous call to __MemoryRegionCheck__() did not
-     * detect a memory fault. If it did, just give up and go home. */
-    if(false == FLAG_MEMFAULT) {
-      /* Check to make sure the memory region has been initialized. If it
-       * hasn't, then the region structure start member will be null. */
-      if(NOTNULLPTR(region_->start)) {
-        cursor = region_->start;
-
-        /* Check option to see if we also need to check an address in the memory
-         * region, if so then set find to the memory entry address. */
-        if(MEMORY_REGION_CHECK_OPTION_W_ADDR == option_) {
-          /* ADDR2ENTRY() calculates the location of the memory entry for the
-           * allocated memory pointed to by the address pointer. */
-          find = ADDR2ENTRY(addr_, region_);
-        }
-
-        /* Start traversing the memory entries in the memory region while the
-         * cursor is null. */
-        while(NOTNULLPTR(cursor)) {
-          /* OKADDR() is a C macro that simply checks that the address, in this
-           * case "cursor", falls within the bounds of the memory region. */
-          if(OKADDR(region_, cursor)) {
-            /* OKMAGIC() compares the memory entry's magic value (i.e., the
-             * magic member of the memory entry structure) to the magic value
-             * calculated by XOR'ing the address of the memory entry with the
-             * MAGIC_CONST. This operation helps ensure we are accessing a valid
-             * memory entry in the memory region being checked. */
-            if(OKMAGIC(cursor)) {
-              if((FREE == cursor->free) || (INUSE == cursor->free)) {
-                blocks += cursor->blocks;
-
-                /* If we are checking for an address in the memory region, then
-                 * see if the cursor matches it and check to make sure the
-                 * memory entry is *NOT* free. If the cursor matches and the
-                 * entry is NOT*
-                 * free then we set "found" to true because we found the entry
-                 * being searched for. Otherwise ignore this step. */
-                if((MEMORY_REGION_CHECK_OPTION_W_ADDR == option_) && (cursor == find) && (INUSE == cursor->free)) {
-                  found = true;
-                }
-
-                cursor = cursor->next;
-              } else {
-                ASSERT;
-
-
-                /* "Houston, we've had a problem." ~ Jim Lovell
-                 *
-                 *
-                 * Set the memfault flag to true because the address we just
-                 * checked does *NOT* have the correct value for free. Something
-                 * is very wrong! */
-                FLAG_MEMFAULT = true;
-                break;
-              }
-            } else {
-              ASSERT;
-
-
-              /* "Houston, we've had a problem." ~ Jim Lovell
-               *
-               *
-               * Set the memfault flag to true because the address we just
-               * checked does *NOT* have the correct magic value. Something is
-               * very wrong! */
-              FLAG_MEMFAULT = true;
-              break;
-            }
+  if(MEMORY_REGION_CHECK_OPTION_WO_ADDR == option_) {
+    while(NOTNULLPTR(cursor)) {
+      if(OKADDR(region_, cursor)) {
+        if(OKMAGIC(cursor)) {
+          if((FREE == cursor->free) || (INUSE == cursor->free)) {
+            blocks += cursor->blocks;
           } else {
-            ASSERT;
-
-
-            /* "Houston, we've had a problem." ~ Jim Lovell
-             *
-             *
-             * Set the memfault flag to true because the address we just checked
-             * is *NOT* inside the memory region. Something is very wrong!
-             */
             FLAG_MEMFAULT = true;
+            ASSERT;
             break;
           }
+        } else {
+          FLAG_MEMFAULT = true;
+          ASSERT;
+          break;
         }
 
-        /* Check to make sure the number of blocks we visited equals the number
-         * of blocks that *SHOULD* be in the memory region which is defined by
-         * CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS. */
-        if(CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS == blocks) {
-          if(((MEMORY_REGION_CHECK_OPTION_WO_ADDR == option_) && (false == FLAG_MEMFAULT)) || ((MEMORY_REGION_CHECK_OPTION_W_ADDR == option_) && (false ==
-            FLAG_MEMFAULT) && (true == found))) {
-            RET_OK;
+        cursor = cursor->next;
+      } else {
+        FLAG_MEMFAULT = true;
+        ASSERT;
+        break;
+      }
+    }
+
+    if(CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS == blocks) {
+      RET_OK;
+    } else {
+      FLAG_MEMFAULT = true;
+      ASSERT;
+    }
+  } else if(MEMORY_REGION_CHECK_OPTION_W_ADDR == option_) {
+    find = ADDR2ENTRY(addr_, region_);
+
+    while(NOTNULLPTR(cursor)) {
+      if(OKADDR(region_, cursor)) {
+        if(OKMAGIC(cursor)) {
+          if((FREE == cursor->free) || (INUSE == cursor->free)) {
+            blocks += cursor->blocks;
+
+            if((find == cursor) && (INUSE == cursor->free)) {
+              found = true;
+            }
           } else {
-            /* Never set FLAG_MEMFAULT = true here! Just because an address
-             * wasn't found does not mean the memory region is corrupt. */
+            FLAG_MEMFAULT = true;
             ASSERT;
+            break;
           }
         } else {
-          ASSERT;
-
-
-          /* "Houston, we've had a problem." ~ Jim Lovell
-           *
-           *
-           * Set the memfault flag to true because the number of blocks visited
-           * does not match the number of blocks the memory region *SHOULD*
-           * have. Something is very wrong!
-           */
           FLAG_MEMFAULT = true;
+          ASSERT;
+          break;
         }
+
+        cursor = cursor->next;
+      } else {
+        FLAG_MEMFAULT = true;
+        ASSERT;
+        break;
+      }
+    }
+
+    if(CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS == blocks) {
+      if(true == found) {
+        RET_OK;
       } else {
         ASSERT;
       }
+    } else {
+      FLAG_MEMFAULT = true;
+      ASSERT;
+    }
+  } else {
+    ASSERT;
+  }
+
+  RET_RETURN;
+}
+
+
+static Return_t __MemoryRegionInit__(volatile MemoryRegion_t *region_) {
+  RET_DEFINE;
+
+  if(NOTNULLPTR(region_)) {
+    region_->start = (MemoryEntry_t *) region_->mem;
+    region_->minAvailableEver = CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS * CONFIG_MEMORY_REGION_BLOCK_SIZE;
+    region_->entrySize = ((HalfWord_t) (sizeof(MemoryEntry_t) / CONFIG_MEMORY_REGION_BLOCK_SIZE));
+
+    if(zero < ((HalfWord_t) (sizeof(MemoryEntry_t) % CONFIG_MEMORY_REGION_BLOCK_SIZE))) {
+      region_->entrySize++;
+    }
+
+    if(OK(__memset__(region_->mem, zero, MEMORY_REGION_SIZE_IN_BYTES))) {
+      region_->start->magic = CALCMAGIC(region_->start);
+      region_->start->free = FREE;
+      region_->start->blocks = CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS;
+      region_->start->next = null;
+      RET_OK;
     } else {
       ASSERT;
     }
@@ -464,43 +451,6 @@ static Return_t __calloc__(volatile MemoryRegion_t *region_, volatile Addr_t **a
   DISABLE_INTERRUPTS();
 
   if(NOTNULLPTR(region_) && NOTNULLPTR(addr_) && (zero < size_)) {
-    /* If we haven't already, calculate how many blocks in size a memory entry
-     * is. Typically this is one, but that may not always be the case. */
-    if(zero == region_->entrySize) {
-      /* Divide the size of the memory entry type by the block size to determine
-       * how many blocks one entry requires. */
-      region_->entrySize = ((HalfWord_t) (sizeof(MemoryEntry_t) / CONFIG_MEMORY_REGION_BLOCK_SIZE));
-
-      /* Calculate the remainder in case part of the memory entry spills over
-       * into another block. */
-      if(zero < ((HalfWord_t) (sizeof(MemoryEntry_t) % CONFIG_MEMORY_REGION_BLOCK_SIZE))) {
-        region_->entrySize++;
-      }
-    }
-
-    /* Check to see if the memory region has been initialized yet, if it hasn't
-     * we need to set the start member of the memory region structure and zero
-     * out the memory and set the first block. */
-    if(NULLPTR(region_->start)) {
-      /* Setting the start member indicates the memory region has been
-       * initialized. */
-      region_->start = (MemoryEntry_t *) region_->mem;
-
-      if(OK(__memset__(region_->mem, zero, MEMORY_REGION_SIZE_IN_BYTES))) {
-        /* CALCMAGIC() calculates the memory entry's magic value (i.e. the magic
-         * member of the memory entry structure) by XOR'ing the address of the
-         * memory entry with the MAGIC_CONST. The magic value is used by
-         * __MemoryRegionCheck__() to check the consistency of the memory
-         * region. */
-        region_->start->magic = CALCMAGIC(region_->start);
-        region_->start->free = FREE;
-        region_->start->blocks = CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS;
-        region_->start->next = null;
-      } else {
-        ASSERT;
-      }
-    }
-
     /* Check the consistency of the memory region before we modify anything. */
     if(OK(__MemoryRegionCheck__(region_, null, MEMORY_REGION_CHECK_OPTION_WO_ADDR))) {
       /* Because the user supplied requested memory in bytes, calculate how many
@@ -510,6 +460,7 @@ static Return_t __calloc__(volatile MemoryRegion_t *region_, volatile Addr_t **a
       if(zero < ((HalfWord_t) (size_ % CONFIG_MEMORY_REGION_BLOCK_SIZE))) {
         requested++;
       }
+
 
       /* Add the number of blocks(s) required by the memory entry to the
        * requested blocks. This is the total number of free blocks that will be
@@ -528,6 +479,7 @@ static Return_t __calloc__(volatile MemoryRegion_t *region_, volatile Addr_t **a
           fewest = cursor->blocks;
           candidate = cursor;
         }
+
 
         /* Keep track of how many free blocks remain as we need to update the
          * statistics for the memory region later. */
@@ -567,6 +519,7 @@ static Return_t __calloc__(volatile MemoryRegion_t *region_, volatile Addr_t **a
           candidate->free = INUSE;
           candidate->blocks = requested;
 
+
           /* Zero out all of the requested blocks (excluding the memory entry).
            */
           if(OK(__memset__(ENTRY2ADDR(candidate, region_), zero, (requested - region_->entrySize) * CONFIG_MEMORY_REGION_BLOCK_SIZE))) {
@@ -583,6 +536,7 @@ static Return_t __calloc__(volatile MemoryRegion_t *region_, volatile Addr_t **a
            * mark the entry as in-use and that's it. */
           candidate->free = INUSE;
 
+
           /* Zero out all of the requested blocks (excluding the memory
            * entry).*/
           if(OK(__memset__(ENTRY2ADDR(candidate, region_), zero, (requested - region_->entrySize) * CONFIG_MEMORY_REGION_BLOCK_SIZE))) {
@@ -595,6 +549,7 @@ static Return_t __calloc__(volatile MemoryRegion_t *region_, volatile Addr_t **a
             ASSERT;
           }
         }
+
 
         /* Update the statistics for the memory region before we are done. */
         region_->allocations++;
@@ -612,6 +567,7 @@ static Return_t __calloc__(volatile MemoryRegion_t *region_, volatile Addr_t **a
   } else {
     ASSERT;
   }
+
 
   /* __calloc__() is done so re-enable interrupts. */
   ENABLE_INTERRUPTS();
@@ -640,6 +596,7 @@ static Return_t __free__(volatile MemoryRegion_t *region_, const volatile Addr_t
       free->free = FREE;
       region_->frees++;
 
+
       /* After freeing memory, call __DefragMemoryRegion__() to consolidate any
        * adjacent free blocks. */
       if(OK(__DefragMemoryRegion__(region_))) {
@@ -653,6 +610,7 @@ static Return_t __free__(volatile MemoryRegion_t *region_, const volatile Addr_t
   } else {
     ASSERT;
   }
+
 
   /* __free__() is done so re-enable interrupts. */
   ENABLE_INTERRUPTS();
@@ -934,6 +892,7 @@ static Return_t __MemGetRegionStats__(const volatile MemoryRegion_t *region_, Me
           (*stats_)->successfulFrees = region_->frees;
           (*stats_)->minimumEverFreeBytesRemaining = region_->minAvailableEver;
 
+
           /* Traverse the memory region to calculate the remaining statistics.
            */
           while(NOTNULLPTR(cursor)) {
@@ -1009,6 +968,7 @@ static Return_t __DefragMemoryRegion__(const volatile MemoryRegion_t *region_) {
           cursor->blocks += merge->blocks;
           cursor->next = merge->next;
 
+
           /* Zero out the block formerly occupied by the memory entry that was
            * merged. */
           if(OK(__memset__(merge, zero, sizeof(MemoryEntry_t)))) {
@@ -1039,10 +999,8 @@ static Return_t __DefragMemoryRegion__(const volatile MemoryRegion_t *region_) {
 
   /* For unit testing only! */
   void __MemoryClear__(void) {
-    __memset__(&heap, 0x0, sizeof(MemoryRegion_t));
-    heap.minAvailableEver = CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS * CONFIG_MEMORY_REGION_BLOCK_SIZE;
-    __memset__(&kernel, 0x0, sizeof(MemoryRegion_t));
-    kernel.minAvailableEver = CONFIG_MEMORY_REGION_SIZE_IN_BLOCKS * CONFIG_MEMORY_REGION_BLOCK_SIZE;
+    __MemoryRegionInit__(&heap);
+    __MemoryRegionInit__(&kernel);
 
     return;
   }
